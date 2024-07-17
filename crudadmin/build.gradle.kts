5402d9aa391bd416d410fc1f2a27a82e847f0ee0
Ayuhamidah <ayuhamidah97@gmail.com>
2024-07-17 17:40:47 +0700

    first commit

---
+++ crudadmin/build.gradle.kts
@@ -0,0 +1,67 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.crudadmin"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.crudadmin"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures{
+        viewBinding{
+            enable = true
+
+        }
+    }
+
+    /*buildFeatures{
+        viewBinding true
+    }*/
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore.ktx)
+    //implementation(com.google.gms)
+    
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
